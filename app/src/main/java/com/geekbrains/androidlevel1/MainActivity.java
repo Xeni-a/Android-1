0221b9ca7bd992353e523629ed1285336e5ba446
Xeni-a <nojhoi.h@yandex.ru>
2021-04-15 13:18:01 +0300

    Android1

---
+++ app/src/main/java/com/geekbrains/androidlevel1/MainActivity.java
@@ -0,0 +1,81 @@
+package com.geekbrains.androidlevel1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private ImageView imageView;
+    private Button button;
+    private EditText editText;
+    private CheckBox checkBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        setNewText();
+        changeTextColor();
+        setHurtToImageView();
+        setAndroidToImageView();
+        setOnBtnClickListener();
+        setCheckboxBehavior();
+    }
+
+    private void setCheckboxBehavior() {
+        boolean isChecked = checkBox.isChecked();
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked1) -> {
+            //...
+        });
+    }
+
+    private void setOnBtnClickListener() {
+        button.setOnClickListener(v -> {
+            String string = editText.getText().toString();
+        textView.setText(string);
+        });
+    }
+
+    private void setAndroidToImageView() {
+        imageView.setImageResource(R.drawable.ic_android_black_24dp);
+    }
+
+    private void setHurtToImageView() {
+//        try {
+//            Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), imageRes);
+//        } catch (NullPointerException ignored) {
+//
+//        }
+////        int imageRes = R.drawable.ic_baseline_favorite_24;
+////
+        imageView.setImageResource(R.drawable.ic_baseline_favorite_24);
+
+    }
+
+    private void initViews() {
+        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button_1);
+        editText = findViewById(R.id.editText);
+        checkBox = findViewById(R.id.checkbox);
+    }
+    private void setNewText(){
+        String appNameStr = getString(R.string.app_name);
+        textView.setText(R.string.hello_Android);
+    }
+    private void changeTextColor() {
+        int colorRes = R.color.purple_500;
+        int color  = ContextCompat.getColor(getApplicationContext(), colorRes);
+        textView.setTextColor(color);
+    }
+}
